b4012b90ed940f9451da7c0bf2b3d2b885635383
vaibhavgoel0230 <vaibhav.goel_cs.aiml19@gla.ac.in>
2021-06-25 17:23:47 +0530

    added sass, get to know styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Board from './components/Board';
+import './styles/root.scss';
 export default () => (
-  <div>
+  <div className="app">
     <h1>TIC TAC TOE</h1>
     <Board />
   </div>
